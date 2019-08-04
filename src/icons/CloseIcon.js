6a067845ea06d64c427a4b24ac56b61810ae4299
TheRealAlan <alancrissey@gmail.com>
2019-08-04 11:40:00 -0400

    Add close icon

---
+++ src/icons/CloseIcon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const CloseIcon = () => {
+  const WIDTH = 357;
+  const HEIGHT = 357;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width={WIDTH}
+      height={HEIGHT}
+      viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
+    >
+      <polygon
+        fill="#fff"
+        points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3 
+			214.2,178.5 		"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
